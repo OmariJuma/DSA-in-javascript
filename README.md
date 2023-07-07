ce9885d6461a723fa8992447d7102dbed02d734e
Omar Juma <jumao2669@gmail.com>
2023-07-07 15:30:33 +0300

    Added the time complexity which is O(n)

---
+++ README.md
@@ -0,0 +1,8 @@
+In the name of Allah the most gracious the most merciful.
+This is my path towards problem solving.
+
+Big-O Guide (Its not 100% correct)
+Calculation  not dependent on input size - O(1)
+1 Loop - O(n)
+2 nested loops - O(n^2)
+3 division inside a loop ie input size is reduced by half - O(logn)
--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,15 @@
 /*fibonacci
 * Problem: Given a number n, find the first n elements of the fibonacci sequence
 * soln: In maths, the fibonacci sequence is a sequence in which each number is the sum of the preceding two ones
+*Big-O= O(n)
 */
 function fibonacci(n) {
-    const fib= [0, 1]
-    for (let x=2; x < n; x++) {
-        fib[x] = fib[x - 1] + fib[x - 2]
+    const fib= [0, 1] // O(1)
+    for (let x=2; x < n; x++) { // O(n)
+        fib[x] = fib[x - 1] + fib[x - 2] // O(n-2)
         console.log(fib);
   }
-  return fib;
+  return fib; //O(1)
 }
 console.log(fibonacci(2))
 console.log(fibonacci(3))
